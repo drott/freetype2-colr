4cf29f9df1523c9dc7a7def5415c3f2b67b9bd9c
Dominik Röttsches <drott@chromium.org>
2020-08-04 13:12:39 +0300

    Documentation naming.

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -4302,7 +4302,7 @@ FT_BEGIN_HEADER
    *
    * @fields:
    *   format ::
-   *     The gradient format for this Paint structure, see @FT_ColorPaint.
+   *     The gradient format for this Paint structure, see @FT_COLR_Paint.
    *
    *   color ::
    *     The color information for this solid paint, see @FT_PaintColor.
@@ -4351,7 +4351,7 @@ FT_BEGIN_HEADER
    *
    * @fields:
    *   format ::
-   *     The gradient format for this Paint structure, see @FT_ColorPaint.
+   *     The gradient format for this Paint structure, see @FT_COLR_Paint.
    *
    *   colorline ::
    *     The @FT_ColorLine information for this pain, i.e. the list of color
@@ -4390,7 +4390,7 @@ FT_BEGIN_HEADER
    *
    * @fields:
    *   format ::
-   *     The gradient format for this Paint structure, see @FT_ColorPaint.
+   *     The gradient format for this Paint structure, see @FT_COLR_Paint.
    *
    *   colorline ::
    *     The @FT_ColorLine information for this pain, i.e. the list of color
@@ -4438,7 +4438,7 @@ FT_BEGIN_HEADER
    *
    * @fields:
    *   format ::
-   *     The gradient format for this Paint structure, see @FT_ColorPaint.
+   *     The gradient format for this Paint structure.
    *
    *   u ::
    *     Union of @FT_PaintSolid, @FT_PaintLinearGradient or
